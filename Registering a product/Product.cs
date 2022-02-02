197119c30d163927acdfc5108a392666f4460df2
truppel1 <89888583+truppel1@users.noreply.github.com>
2022-02-02 02:22:44 -0300

    Add files via upload

---
+++ Registering a product/Product.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Exercicios_com_classe
+{
+    internal class Product
+    {
+        CultureInfo CI = CultureInfo.InvariantCulture;
+
+        public string name;
+        public double price;
+        public int quant;
+
+        public double Totalvaluestok()
+        {
+            return price * quant;
+        }
+
+        public void Addproducts()
+        {
+            int add = int.Parse(Console.ReadLine());
+            quant = quant + add;
+
+        }
+        public void Removeproducts()
+        {
+            int addI = int.Parse(Console.ReadLine());
+            quant = quant + addI;
+        }
+
+        public override string ToString()
+        {
+            return "Product " + name + ", price " + price
+                + "U$, quantity in stok " + quant +
+                ", Total value in stok " + Totalvaluestok().ToString("F2", CI) + "U$.";
+        }
+
+    }
+}
---
+++ Registering a product/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicios_com_classe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            Product product = new Product();
+
+            Console.Write("Type the product name: ");
+            product.name = Console.ReadLine();
+            Console.Write("Type the product price: ");
+            product.price = double.Parse(Console.ReadLine(), CI);
+            Console.Write("Type the product quantity: ");
+            product.quant = int.Parse(Console.ReadLine());
+            // DATA INSERT OF THE PRODUCT
+
+            Console.WriteLine("\n" + product);
+
+            // ADDING A PRODUCT
+            Console.Write("\nType quantity to be add: ");
+            product.Addproducts();
+            Console.WriteLine("\n " + product);
+
+            // REMOVING A PRODUCT
+           
+            Console.Write("\nType quantity to be remove: ");
+            product.Removeproducts();
+            Console.WriteLine("\n" + product);
+
+        }
+    }
+}
