b3a09ee98cad27102a801f0f99818e5f894fc3ba
truppel1 <89888583+truppel1@users.noreply.github.com>
2022-02-08 22:33:01 -0300

    Readme

---
+++ Employee/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Exercicios
+{
+    internal class Employee
+    {
+        CultureInfo CI = CultureInfo.InvariantCulture;
+        public string name;
+        public double salary;
+        public double tax;
+        public double percent;
+
+
+        public double NetSalary()
+        {
+            return salary - tax + percent;
+        }
+        public void IncreaseSalary(double _percent)
+        {
+            percent = (salary * _percent) / 100;
+        }
+        public override string ToString()
+        {
+            return "Employee: " + name + ", U$" +
+                NetSalary().ToString("F2", CI);
+        }
+
+
+    }
+}
---
+++ Employee/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicios
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+            string name;
+            double salary, tax;
+            double percent;
+
+            Employee X = new Employee();
+
+            Console.Write("Name: ");
+            X.name = Console.ReadLine();
+            Console.Write("Gross Salary: ");
+            X.salary = double.Parse(Console.ReadLine(), CI);
+            Console.Write("Taxes: ");
+            X.tax = double.Parse(Console.ReadLine(), CI);
+            //DATA COLLECTED
+
+            Console.WriteLine("\n" + X + "\n");
+
+            Console.Write("Enter the percentage to be increse at the salary: ");
+            percent = double.Parse(Console.ReadLine(), CI);
+            X.IncreaseSalary(percent);
+            // INCREASE SALARY
+
+            Console.WriteLine("\n" + X);
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
