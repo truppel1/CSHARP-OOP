0e352bcea02fb64794391ba95c05fb14f195893d
truppel1 <89888583+truppel1@users.noreply.github.com>
2022-02-08 22:25:10 -0300

    readme

---
+++ Convert Currency/Currency/Convert.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Currency
+{
+    internal class Convert
+    {
+
+        public static double ConvertC(double dolar, double _price)
+        {
+            return (((dolar * _price) * 6) / 100) + (dolar * _price);
+        }
+
+    }
+}
---
+++ Convert Currency/Currency/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace Currency
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            Console.Write("What's the current dolar coin: ");
+            double price = double.Parse(Console.ReadLine(), CI);
+            Console.Write("How much Dolars youre going to buy: ");
+            double dolar = double.Parse(Console.ReadLine(), CI);
+
+            Console.WriteLine("Total value to be payed in R$ = " +
+                Convert.ConvertC(dolar, price).ToString("F2", CI));
+
+        }
+    }
+}
