63ded59fa4883d3f67db7ec8a31cb056a560e7ae
truppel1 <89888583+truppel1@users.noreply.github.com>
2022-02-08 21:59:42 -0300

    Final

---
+++ Bank Deposits Withdraw/Account.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Bank_Deposits
+{
+    internal class Account
+    {
+        CultureInfo CI = CultureInfo.InvariantCulture;
+
+        public int Number { get; private set; }
+        public double Balance { get; private set; }
+        public string Name;
+
+        public Account(int number, string name)
+        {
+            Number = number;
+            Name = name;
+            Balance = 0.0;
+        }
+        public Account(int number, string name, double deposit)
+        {
+            Number = number;
+            Name = name;
+            Balance = deposit;
+        }
+
+        public void DepositValue(double deposit)
+        {
+            Balance = Balance + deposit;
+        }
+
+        public void Withdraw(double withdraw)
+        {
+            Balance = Balance - withdraw - 5;
+        }
+
+        public override string ToString()
+        {
+            return "Account Data Update: " +
+                   "\nAccount " + Number + ", Owner: " + Name
+                + "  Balance: U$" + Balance.ToString("F2", CI);
+        }
+    }
+}
---
+++ Bank Deposits Withdraw/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+
+namespace Bank_Deposits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+            double deposit = 0;
+
+            Console.Write("Enter the account number: ");
+            int acc = int.Parse(Console.ReadLine());
+            Console.Write("Enter the owner full name: ");
+            string name = Console.ReadLine();
+            Console.Write("Would you like to do the first deposit (Y/N): ");
+            string x = Console.ReadLine();
+            if (x == "Y" || x == "y")
+            {
+                Console.Write("Enter the initial amount: ");
+                deposit = double.Parse(Console.ReadLine(), CI);
+            }
+
+            Account X = new Account(acc, name, deposit); //ACCOUNT CREATED
+            Console.WriteLine();
+            Console.WriteLine(X);
+
+            Console.Write("\nEnter a value to deposit: ");
+            deposit = double.Parse(Console.ReadLine(), CI);
+            X.DepositValue(deposit);
+
+            Console.Write(X);
+
+            Console.WriteLine();
+            Console.Write("\nEnter a value to withdraw: ");
+            double withdraw = double.Parse(Console.ReadLine(), CI);
+            X.Withdraw(withdraw);
+            Console.WriteLine(X);
+        }
+    }
+}
