75fd4d0466b99103fbb35bc57d4c090a4796e8e1
truppel1 <89888583+truppel1@users.noreply.github.com>
2022-02-10 03:39:41 -0300

    Readme

---
+++ Matrix exercise/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Array_bi_dimensional
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] xy = Console.ReadLine().Split(' ');
+            int N = int.Parse(xy[0]);
+            int M = int.Parse(xy[1]);
+
+            int[,] mat = new int[N, M];
+
+            for (int i = 0; i < N; i++)
+            {
+                string[] values = Console.ReadLine().Split(' ');
+                for (int j = 0; j < M; j++)
+                {
+                    mat[i, j] = int.Parse(values[j]);
+                }
+            } //COLLECTED DATA
+
+            int x = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < N; ++i)
+            {
+                for (int j = 0; ++j < M; j++)
+                {
+                    if (mat[i, j] == x)
+                    {
+                        Console.WriteLine("Position " + i + "," + j + ":");
+
+                        if (j > 0)
+                        {
+                            Console.WriteLine("Left: " + mat[i, j - 1]);
+                        }
+                        if (i > 0)
+                        {
+                            Console.WriteLine("Up: " + mat[i - 1, j]);
+                        }
+                        if (j < N - 1)
+                        {
+                            Console.WriteLine("Right: " + mat[i, j + 1]);
+                        }
+                        if (i < M - 1)
+                        {
+                            Console.WriteLine("Down: " + mat[i + 1, j]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Matrix exercise/Readme.txt
@@ -0,0 +1,7 @@
+Make a program to read two integers M and N, and then read
+a matrix of M rows by N columns containing integers,
+there may be.
+
+Then read an integer X that belongs to the matrix.
+For each occurrence of X, show the values ​​at
+left, above, right and below X, if any.
